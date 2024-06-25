7ff2f58d184df84c96fad5aaa585ea59baaa8f45
nileshcode1 <itsofficialcode@gmail.com>
2024-06-25 14:39:30 +0530

    update

---
+++ src/Sections/Testimonials/index.js
@@ -0,0 +1,115 @@
+import React, { lazy } from "react";
+import styled from "styled-components";
+
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
+const Card = lazy(() => import("../../components/Card/index"));
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 5rem 0;
+`;
+
+const Title = styled.h1`
+  color: #0a0b10;
+  display: inline-block;
+  font-size: calc(1rem + 1.5vw);
+  margin-top: 1.5rem;
+  position: relative;
+  &::before {
+    content: "";
+    height: 1px;
+    width: 50%;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0.5rem);
+    /* or 100px */
+    border-bottom: 2px solid var(--purple);
+  }
+`;
+
+const Carousal = styled.div`
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media only Screen and (max-width: 40em) {
+    width: 90vw;
+    .slick-slider .slick-arrow {
+      display: none;
+    }
+  }
+  .slick-slider .slick-arrow:before {
+    color: #0a0b10;
+    font-size: 1.5rem;
+    @media only Screen and (max-width: 40em) {
+      display: none;
+    }
+  }
+  .slick-slider .slick-dots button:before {
+    color: #0a0b10;
+    font-size: 1.5rem;
+  }
+  .slick-slide.slick-active {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Section>
+      <Title>Few good words about us!</Title>
+      <Carousal>
+        <Slider {...settings}>
+          <Card
+            text="Annapurna & Sons transformed our storefront with vibrant, engaging displays. Our foot traffic has increased significantly. Highly recommend!"
+            name="Amrita"
+            image="avatar-1"
+          />
+
+          <Card
+            text="Our new digital menu boards from annapurna & Sons have made a huge difference. They are easy to update and have helped boost our sales"
+            name="Suresh (Apollo Pharmacy)"
+            image="avatar-2"
+          />
+
+          <Card
+            text="The displays of Annapurna & Sons are user-friendly, and the content management system is intuitive. Excellent service and support!"
+            name="Fatima (Vivo Store)"
+            image="avatar-3"
+          />
+
+          <Card
+            text="The team at Annapurna & Sons was fantastic to work with. They understood our vision and delivered a solution. Our brand visibility has never been better!"
+            name="R.L Mehta (Axis Bank)"
+            image="avatar-4"
+          />
+        </Slider>
+      </Carousal>
+    </Section>
+  );
+};
+
+export default Testimonials;
